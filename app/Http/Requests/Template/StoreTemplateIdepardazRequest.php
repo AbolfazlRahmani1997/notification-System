23563d6b6d31e39a521ef0e916877bfeaedd997d
rahmani <abolfazlrahmani1997@gmail.com>
2024-08-27 10:55:10 +0330

    Completed Template Controller And Route

---
+++ app/Http/Requests/Template/StoreTemplateIdepardazRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Template;
+
+use App\Enums\SMSPanelTypeEnum;
+use App\Http\Requests\Traits\UseApiRequest;
+use App\Rules\ParameterValue;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTemplateIdepardazRequest extends FormRequest
+{
+    use UseApiRequest;
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required'],
+            'provider' => ['required', 'integer', 'in:' . implode(',', SMSPanelTypeEnum::values())],
+            "template_name" => ['required', 'string'],
+            'parameter'=>['array'],
+            "parameters.*"=>[ 'regex:/^PARAMETER\d+$/'],
+            'parameters_title'=>['array',new ParameterValue()],
+        ];
+    }
+}
